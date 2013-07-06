85d803438c4ee92f03f474d8d44de76e209352ae
Jucy Zhang <zaoxing.zhang@gmail.com>
2013-07-07 00:36:56 +0800

    so easy to add new Filters!

---
+++ src/com/jucyzhang/rsfilter/ScreenRSFilter.java
@@ -0,0 +1,28 @@
+package com.jucyzhang.rsfilter;
+
+import android.content.Context;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+
+public class ScreenRSFilter extends BaseRSFilter<ScriptC_screen> {
+
+  public static final String DST = "dst";
+  public static final String SRC = "src";
+
+  @Override
+  protected ScriptC_screen onCreateScript(Context context, RenderScript rs) {
+    return new ScriptC_screen(rs);
+  }
+
+  @Override
+  protected Allocation onProcessImage(Context context, RenderScript rs) {
+    Allocation dst = getAllocation(context, rs, DST);
+    Allocation src = getAllocation(context, rs, SRC);
+    Allocation out = Allocation.createTyped(rs, dst.getType());
+    ScriptC_screen screen = getScript();
+    screen.bind_src(src);
+    screen.invoke_filter(screen, dst, out);
+    return out;
+  }
+
+}
